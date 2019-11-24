fd59d1854399f37258a9b2ccb7eba2e83ec0e2bf
Daylen Yang <services@daylenyang.com>
2019-11-23 18:02:58 -0800

    iMessage data reader: join on handle table to get phone number

--- data_reader_imessage.py
+++ data_reader_imessage.py
@@ -5,6 +5,7 @@ import sqlite3
 from constants import *
 
 def read_imessage(path):
+	# useful resource: https://github.com/dsouzarc/iMessageAnalyzer/blob/master/Random%20SQLite%20Commands.txt
 	conn = sqlite3.connect(path)
 	c = conn.cursor()
 	c2 = conn.cursor()
@@ -12,7 +13,7 @@ def read_imessage(path):
 	chat_ids = c.execute("SELECT ROWID FROM chat")
 	for chat_id in chat_ids:
 		print('CHAT ID', chat_id)
-		messages = c2.execute("SELECT text, handle_id, date, is_from_me FROM message messageT INNER JOIN chat_message_join chatMessageT ON (chatMessageT.chat_id=" + str(chat_id[0]) + ") AND messageT.ROWID=chatMessageT.message_id ORDER BY messageT.date")
+		messages = c2.execute("SELECT text, handleT.id, date, is_from_me FROM message messageT INNER JOIN chat_message_join chatMessageT ON (chatMessageT.chat_id=" + str(chat_id[0]) + ") AND messageT.ROWID=chatMessageT.message_id INNER JOIN handle handleT ON handleT.ROWID = messageT.handle_id ORDER BY messageT.date")
 		for msg in messages:
 			print('Message:',msg)
 	conn.close()
