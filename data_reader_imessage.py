1b794a50fc4265532f186550cb77778b6e91c824
Daylen Yang <services@daylenyang.com>
2019-11-23 18:38:39 -0800

    iMessage data reader: use proto

--- data_reader_imessage.py
+++ data_reader_imessage.py
@@ -11,12 +11,25 @@ def read_imessage(path):
 	c2 = conn.cursor()
 	# get all the chat IDs
 	chat_ids = c.execute("SELECT ROWID FROM chat")
+	inbox = chat_pb2.Inbox()
 	for chat_id in chat_ids:
-		print('CHAT ID', chat_id)
+		# print('CHAT ID', chat_id)
+		conversation = chat_pb2.Conversation()
+		# TODO fill in name, participants
+		conversation.group_name = str(chat_id[0])
 		messages = c2.execute("SELECT text, handleT.id, date, is_from_me FROM message messageT INNER JOIN chat_message_join chatMessageT ON (chatMessageT.chat_id=" + str(chat_id[0]) + ") AND messageT.ROWID=chatMessageT.message_id INNER JOIN handle handleT ON handleT.ROWID = messageT.handle_id ORDER BY messageT.date")
 		for msg in messages:
-			print('Message:',msg)
+			if msg[0] is None:
+				print('skipping empty message', msg)
+				continue
+			message_proto = conversation.message.add()
+			message_proto.sender_name = msg[1]
+			message_proto.timestamp = msg[2]
+			message_proto.content_type = chat_pb2.Message.CT_TEXT
+			message_proto.content = msg[0]
+		inbox.conversation.extend([conversation])
 	conn.close()
+	return inbox
 
 def main():
 	inbox = read_imessage(IMESSAGE_IMPORT_PATH)
